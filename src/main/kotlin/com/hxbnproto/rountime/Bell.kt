55f07f3f114a992e9f1b7b35adbb3bed67e1b222
hxbn-proto <chernik.lut@gmail.com>
2020-11-29 11:47:27 +0100

    Changed src folder structure
    Added resources folder

--- src/main/kotlin/com/hxbnproto/rountime/Bell.kt
+++ src/main/kotlin/com/hxbnproto/rountime/Bell.kt
@@ -2,10 +2,11 @@ package com.hxbnproto.rountime
 
 import javafx.scene.media.Media
 import javafx.scene.media.MediaPlayer
-import java.io.File
 
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class Bell : Thread() {
-    private val player: MediaPlayer = MediaPlayer(Media(File("ring.mp3").toURI().toURL().toString()))
+    private val player: MediaPlayer =
+        MediaPlayer(Media(javaClass.classLoader.getResource("ring.mp3").toURI().toURL().toString()))
 
     fun ring() {
         Bell().start()
